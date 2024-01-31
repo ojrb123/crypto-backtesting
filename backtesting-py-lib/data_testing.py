1bcb314a132fb127a6afd51c04e1b0509edb6b62
rajan-ste <145256357+rajan-ste@users.noreply.github.com>
2024-01-31 17:04:22 +1300

    more data

--- backtesting-py-lib/data_testing.py
+++ backtesting-py-lib/data_testing.py
@@ -1,6 +1,6 @@
 import csv
 
-with open('./data/LINK-USDT60-HULL.csv', 'r') as csv_file:
+with open('./data/LINK-USDT15-HULL-LSMA-PSAR.csv', 'r') as csv_file:
     reader = csv.reader(csv_file)
 
     for row in reader:
