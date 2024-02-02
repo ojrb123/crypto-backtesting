e35bd4e46235a56890bca4b1a651044c98d57bc4
rajan-ste <145256357+rajan-ste@users.noreply.github.com>
2024-02-02 19:17:55 +1300

    more data, remove redundant files

--- Simulator.py
+++ Simulator.py
@@ -9,26 +9,25 @@ LINKdf1 = './data/LINKUSDT15-SUPER-1.csv'
 LINKdf2 = './data/LINKUSDT15-SUPER-2.csv'
 SNXdf1 = './data/SNXUSDT15-SUPER-1.csv'
 SNXdf2 = './data/SNXUSDT15-SUPER-2.csv'
-LTCdf = './data/LTC-USDT15.csv'
-DOTdf = './data/DOT-USDT15.csv'
-AVAXdf = './data/AVAX-USDT15.csv'
+LTCdf1 = './data/LTCUSDT15-SUPER-1.csv'
+DOTdf1 = './data/DOTUSDT15-SUPER-1.csv'
+DOTdf2 = './data/DOTUSDT15-SUPER-2.csv'
+AVAXdf1 = './data/AVAXUSDT15-SUPER-1.csv'
 
 # custom vars
-TAKE_PROFIT = 10
-STOP_LOSS = -5
-BALANCE = 100
+
 
 starting_time = time.time()
 average_stats = AverageStats()
 
-def run_simulation(df):
+def run_simulation(df, tp, sl, bal):
     with open(df, 'r') as csv_file:
         reader = csv.reader(csv_file)
         next(reader)  # Skip header
 
         # Initialize the sliding window for HMA calculation
         HMA_window = deque(maxlen=11)
-        ST_up_window = deque(maxlen=10)  
+          
 
         # Init the PSAR array
         PSAR_data = get_PSAR_Data(df)
@@ -68,42 +67,45 @@ def run_simulation(df):
                 if BUY:
                     percent_change = (float(row[4]) - BUY_PRICE) / BUY_PRICE * 100
 
-                    if percent_change >= TAKE_PROFIT:
+                    if percent_change >= tp:
                         WIN += 1
                         BUY = False
                         TRIALS += 1
                     
-                    if percent_change <= STOP_LOSS:
+                    if percent_change <= sl:
                         BUY = False
                         TRIALS += 1
                         
                 prev_hma_state = curr_hma_state
 
         TO_BEAT = (current_close - first_close) / first_close * 100
-        EXPECTED, TRADES_PER_DAY, WINRATE, WINS_PER_DAY, DAILY_VALUE, TO_BEAT_BALANCE, END_BALANCE = calc_stats(WIN, TRIALS, TAKE_PROFIT, STOP_LOSS, CANDLES, TO_BEAT, BALANCE)
+        EXPECTED, TRADES_PER_DAY, WINRATE, WINS_PER_DAY, DAILY_VALUE, TO_BEAT_BALANCE, END_BALANCE = calc_stats(WIN, TRIALS, tp, sl, CANDLES, TO_BEAT, bal)
         print_stats(CANDLES, TRIALS, WIN, WINRATE, TRADES_PER_DAY, WINS_PER_DAY, EXPECTED, DAILY_VALUE, TO_BEAT_BALANCE, END_BALANCE)
         average_stats.update_stats(CANDLES, TRIALS, WIN, WINRATE, TRADES_PER_DAY, WINS_PER_DAY, EXPECTED, DAILY_VALUE, TO_BEAT_BALANCE, END_BALANCE)
 
 print("---------------LINK---------------")
-run_simulation(LINKdf1)
+run_simulation(LINKdf1, 5, -2, 1000)
 
 print("---------------LINK---------------")
-run_simulation(LINKdf2)
+run_simulation(LINKdf2, 5, -2, 1000)
 
 print("---------------SNX---------------")
-run_simulation(SNXdf1)
+run_simulation(SNXdf1, 5, -2, 1000)
 
 print("---------------SNX---------------")
-run_simulation(SNXdf2)
+run_simulation(SNXdf2, 5, -2, 1000)
 
-# print("---------------LTC---------------")
-# run_simulation(LTCdf)
+print("---------------LTC---------------")
+run_simulation(LTCdf1, 5, -2, 1000)
 
-# print("---------------DOT---------------")
-# run_simulation(DOTdf)
+print("---------------DOT---------------")
+run_simulation(DOTdf1, 5, -2, 1000)
 
-# print("---------------AVAX---------------")
-# run_simulation(AVAXdf)
+print("---------------DOT---------------")
+run_simulation(DOTdf2, 5, -2, 1000)
+
+print("---------------AVAX---------------")
+run_simulation(AVAXdf1, 5, -2, 1000)
 
 averages = average_stats.calculate_averages()
 print_averages(averages)
